a176924da51145029d9072f8523faf63cb0b1d94
Anthony Tresontani <tresontani@gmail.com>
2012-06-20 09:50:04 +0200

    Fix the typo in view

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='django-async-reports',
-      version='0.2.00',
+      version='0.2.0',
       long_description =read('README.txt'),
       description='Reporting for django',
       keywords = "CSV Django reporting",
